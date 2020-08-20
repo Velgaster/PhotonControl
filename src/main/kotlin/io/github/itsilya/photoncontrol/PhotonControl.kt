b5b5c6db9bd851906c73633446be84b3d71fcc21
Velgaster <67159982+Velgaster@users.noreply.github.com>
2020-08-20 14:03:49 +0200

    client update command
    only updates the current chunk

--- src/main/kotlin/io/github/itsilya/photoncontrol/PhotonControl.kt
+++ src/main/kotlin/io/github/itsilya/photoncontrol/PhotonControl.kt
@@ -14,6 +14,7 @@ class PhotonControl : ModInitializer {
         CommandRegistrationCallback.EVENT.register(CommandRegistrationCallback { dispatcher, _ ->
             PhotonControlCommand(dispatcher)
         })
+
     }
 
 }
--- src/main/kotlin/io/github/itsilya/photoncontrol/command/PhotonControlCommand.kt
+++ src/main/kotlin/io/github/itsilya/photoncontrol/command/PhotonControlCommand.kt
@@ -2,7 +2,12 @@ package io.github.itsilya.photoncontrol.command
 
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.context.CommandContext
+import io.github.itsilya.photoncontrol.PhotonControl
 import io.github.itsilya.photoncontrol.mixins.LightingProviderAccessor
+import net.fabricmc.fabric.mixin.event.lifecycle.MinecraftServerMixin
+import net.minecraft.client.MinecraftClient
+import net.minecraft.network.packet.s2c.play.LightUpdateS2CPacket
+import net.minecraft.server.MinecraftServer
 import net.minecraft.server.command.CommandManager
 import net.minecraft.server.command.ServerCommandSource
 import net.minecraft.server.world.ServerWorld
@@ -11,38 +16,45 @@ import net.minecraft.util.math.ChunkPos
 import net.minecraft.world.chunk.light.ChunkLightProvider
 import net.minecraft.world.chunk.light.LightingProvider
 
+
 class PhotonControlCommand(dispatcher: CommandDispatcher<ServerCommandSource>) {
     init {
         val builder = CommandManager.literal("pc")
-
-        builder.then(
-            CommandManager.literal("test").executes(::test)
+        val commandMap = mapOf<String, (CommandContext<ServerCommandSource>)->Int >(
+                "set" to ::setlight,
+                "update" to ::updateclient
         )
-
+        commandMap.forEach{
+            builder.then(CommandManager.literal(it.key).executes(it.value))
+        }
         dispatcher.register(builder)
     }
 
-    private fun test(ctx: CommandContext<ServerCommandSource>): Int {
+
+    private fun updateclient(ctx: CommandContext<ServerCommandSource>): Int {
         val player = ctx.source.player
         val world: ServerWorld = player.serverWorld
         val pos = player.blockPos
         val chunkPos = ChunkPos(pos)
+        val lightingProvider: LightingProvider = world.lightingProvider
+        world.chunkManager.sendToNearbyPlayers(player, LightUpdateS2CPacket(chunkPos, lightingProvider, true))
+        ctx.source.sendFeedback(LiteralText("Client Updated."), false)
+        return 1
+    }
+
+    private fun setlight(ctx: CommandContext<ServerCommandSource>): Int {
+        val player = ctx.source.player
+        val world: ServerWorld = player.serverWorld
+        val pos = player.blockPos
 
         val lightingProvider: LightingProvider = world.lightingProvider
         val chunkLightingProvider: ChunkLightProvider<*, *> =
             (lightingProvider as LightingProviderAccessor).chunkLightProvider
 
-//        lightingProvider.setLightEnabled(chunkPos, true)
-//        lightingProvider.setRetainData(chunkPos, true)
-//        lightingProvider.addLightSource(pos, 15)
-//        lightingProvider.updateSectionStatus(ChunkSectionPos.from(pos), true)
-//        lightingProvider.setColumnEnabled(chunkPos, true)
-//        world.chunkManager.markForUpdate(pos)
-
         chunkLightingProvider.addLightSource(pos, 15)
-        world.chunkManager.markForUpdate(pos)
+        chunkLightingProvider.doLightUpdates(100,false,true)
 
-        ctx.source.sendFeedback(LiteralText("Test Done"), false)
+        ctx.source.sendFeedback(LiteralText("light source set at $pos"), false)
         return 1
     }
 }
